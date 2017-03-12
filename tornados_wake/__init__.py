676bb6b3a5129db1ec3c4c2e67d0a45dfaeb0fbf
Jon Coe <jon@seatgeek.com>
2017-03-12 14:32:09 -0400

    add HTTP methods behavior / keyword

--- tornados_wake/__init__.py
+++ tornados_wake/__init__.py
@@ -1,23 +1,46 @@
 import json
+from operator import itemgetter
+
+from tornado.web import RequestHandler
 
 
 def get_routes_list(application, excludes=frozenset()):
-    return sorted([i._path for i in application.handlers[0][1] if i._path not in excludes])
+    return sorted(((i._path, _methods_from_handler_class(i.handler_class))
+            for i in application.handlers[0][1]
+            if i._path not in excludes),
+           key=itemgetter(0))
 
 
-def get_route_tree_dict(application, excludes=frozenset()):
-    routes_split = [route.split('/')[1:] for route in get_routes_list(application, excludes=excludes)]
-    return _make_tree_from_list(routes_split)
+def get_route_tree_dict(routes_list):
+    d = {}
+    for route, methods in routes_list:
+        methods = list(methods)
+        route_split = route.split('/')[1:]  # eliminate empty string in leading position
+        subdict = d
+        for depth, segment in enumerate(route_split):
+            if '/' + segment not in subdict:
+                is_last_segment = depth == len(route_split) - 1
+                subdict['/' + segment] = {'': methods} if is_last_segment else {}
+            subdict = subdict['/' + segment]
+    return d
 
 
-def make_route_handler(base_handler,
-                       excludes=frozenset(), pretty_keyword='pretty', jsonify=True, respond_func_str='finish'):
+def make_route_handler(base_handler=RequestHandler,
+                       excludes=frozenset(),
+                       tree_keyword='tree', tree_default=False,
+                       methods_keyword='methods', methods_default=True,
+                       jsonify=True, respond_func_str='finish'):
 
     class RouteHandler(base_handler):
 
         def get(self):
-            if pretty_keyword and self.get_argument(pretty_keyword, None):
-                payload = {'routes': get_route_tree_dict(self.application, excludes=excludes)}
+            include_methods = methods_keyword and self.get_argument(methods_keyword, methods_default)
+            make_tree = tree_keyword and self.get_argument(tree_keyword, tree_default)
+
+            routes = get_routes_list(self.application, excludes=excludes)
+
+            if make_tree:
+                payload = {'routes': get_route_tree_dict(routes)}
             else:
                 payload = {'routes': get_routes_list(self.application, excludes=excludes)}
             respond_func = getattr(self, respond_func_str)
@@ -27,21 +50,26 @@ def make_route_handler(base_handler,
     return RouteHandler
 
 
-def _make_tree_from_list(list_split):
-    d = {'/': {}}
-    for route in list_split:
-        subdict = d['/']
-        for depth, segment in enumerate(route):
-            if segment not in subdict:
-                subdict[segment] = {'': {}} if depth == len(route) - 1 else {}
-            subdict = subdict[segment]
-    _prune(d)
-    return d
+def _methods_from_handler_class(hc):
+    """
+    Given a handler class, return supported methods.
+    Will *not* work for any methods set dynamically (like setattr in the `prepare` or something)
+
+    :param hc: Handler class
+    :return: set of implemented methods ex. {'GET', 'POST' 'PUT'}
+    """
+
+    BASES = {RequestHandler, object}
+    candidates = hc.SUPPORTED_METHODS
+    result = set()
+    for candidate in candidates:
+        if _class_has_method(hc, candidate, BASES):
+            result.add(candidate)
+    return result
 
 
-def _prune(d):
-    for k, v in d.items():
-        if v == {'': {}} or v == {}:
-            d[k] = None
-        elif isinstance(v, dict):
-            _prune(v)
+def _class_has_method(klass, method, bases):
+    if klass in bases:
+        return False
+    else:
+        return method.lower() in klass.__dict__ or any(_class_has_method(b, method, bases) for b in klass.__bases__)
