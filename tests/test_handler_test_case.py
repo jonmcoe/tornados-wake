402c33158a5173e776202d5ab75ef41b931b402f
Jon Coe <jon@seatgeek.com>
2017-08-13 12:42:04 -0400

    kill extra line

--- tests/test_handler_test_case.py
+++ tests/test_handler_test_case.py
@@ -5,7 +5,6 @@ from tests.base import BaseHTTPTestCase
 
 class TestHandlerTestCase(BaseHTTPTestCase):
 
-
     def test_routes_list_with_methods(self):
         x = self.fetch('/routes')
         expected = '{"routes": [["/", ["GET"]], ["/assets/%s", ["GET"]], ["/pets", ["GET", "POST"]], ["/pets/%s", ["GET", "DELETE", "PATCH", "PUT"]], ["/pets/%s/pictures", ["GET", "POST"]], ["/pets/%s/pictures/%s", ["GET", "DELETE", "PATCH", "PUT"]], ["/routes", ["GET"]], ["/toys", ["GET", "POST"]], ["/toys/%s", ["GET", "DELETE", "PATCH", "PUT"]]]}'
