f046b4b30dfb6cb416f1bc548c741467171256e7
Jon Coe <jon@seatgeek.com>
2017-08-13 13:15:59 -0400

    update tests for py3

--- tests/test_handler_test_case.py
+++ tests/test_handler_test_case.py
@@ -7,7 +7,7 @@ class TestHandlerTestCase(BaseHTTPTestCase):
 
     def test_routes_list_with_methods(self):
         x = self.fetch('/routes')
-        expected = '{"routes": [["/", ["GET"]], ["/assets/%s", ["GET"]], ["/pets", ["GET", "POST"]], ["/pets/%s", ["GET", "DELETE", "PATCH", "PUT"]], ["/pets/%s/pictures", ["GET", "POST"]], ["/pets/%s/pictures/%s", ["GET", "DELETE", "PATCH", "PUT"]], ["/routes", ["GET"]], ["/toys", ["GET", "POST"]], ["/toys/%s", ["GET", "DELETE", "PATCH", "PUT"]]]}'
+        expected = b'{"routes": [["/", ["GET"]], ["/assets/%s", ["GET"]], ["/pets", ["GET", "POST"]], ["/pets/%s", ["GET", "DELETE", "PATCH", "PUT"]], ["/pets/%s/pictures", ["GET", "POST"]], ["/pets/%s/pictures/%s", ["GET", "DELETE", "PATCH", "PUT"]], ["/routes", ["GET"]], ["/toys", ["GET", "POST"]], ["/toys/%s", ["GET", "DELETE", "PATCH", "PUT"]]]}'
         self.assertEqual(x.body, expected)
 
     @skip
