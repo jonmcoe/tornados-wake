3a331a57870d681aaab62805aa79606a9a8b5a83
Jon Coe <jon@seatgeek.com>
2017-08-13 14:10:25 -0400

    upgrade version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="tornados-wake",
-    version="0.0.1",
+    version="0.1.1",
     author="Jon Coe",
     author_email="jonmcoe@gmail.com",
     description="Tornado's Wake: Handler and tools for inspecting routes of a tornado server",
@@ -19,6 +19,6 @@ setup(
     url="https://github.com/jonmcoe/tornados-wake",
     packages=['tornados_wake'],
     long_description=__doc__,
-    install_requires=['tornado'],
+    install_requires=['tornado>=3'],
     tests_require=['nose']
 )
